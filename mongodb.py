4867706aeeb264853d7e47f05ed969cd1903be77
evilsadko <nerevaren@mail.ru>
2021-07-20 15:37:39 +0300

    update

--- mongodb.py
+++ mongodb.py
@@ -5,6 +5,7 @@ import gridfs
 import cv2
 import numpy as np
 import uuid
+import time
 from bson.objectid import ObjectId
 
 def imgs(x):
@@ -99,65 +100,35 @@ class DataBase(object):
          for idb in self.client.database_names():
              print (idb, "Имя базы данных")
              
-def images_data():
-        img = cv2.imread("10700181-Image-1.jpeg")
-        S = img.shape
-        print (img.shape)
-        _, img = cv2.imencode(".jpeg",img)
-        img = img.tostring()
-        #imgs(img)
-        db = DataBase("anpr")
-        #imageID = db.file.put(img.tostring(), encoding='utf-8') # ложу файлы
-        #print (imageID)#
-        #------------------_>
-        #print (db.db['fs.files'].find_one())
-        #print (list(db.db['fs.files'].find()))
-        #print (list(db.file.find()))#(db.file.list())
-        #------------------_>
-        #img = db.file.find_one({"_id":"5f480851b2c79a1ff0b884ca"})
-        #img = db.db['fs.files'].get("5f480851b2c79a1ff0b884ca")
-        #img = db.file.get(ObjectId("5f480851b2c79a1ff0b884ca")).read()
-        #print (img)
-        nparr = np.fromstring(img, np.uint8)
-        #nparr = np.frombuffer(img, np.uint8)
-        #img = np.reshape(nparr, S)
-        #nparr = nparr.reshape(1, -1)
-        img_t = cv2.imdecode(nparr, cv2.IMREAD_COLOR) #cv2.IMREAD_UNCHANGED
-        print (nparr, img_t)
-        imgs(img_t)
  
-#def imgs_data_write():
-#        db = DataBase("anpr")
-#        img = cv2.imread("10700181-Image-1.jpeg")
-#        imageID = db.file.put(img.tostring(), encoding='utf-8')
-#        dump = {
-#            'name': 'myTestSet',
-#            'images': [
-#                {
-#                    'imageID': imageID,
-#                    'shape': img.shape,
-#                    'dtype': str(img.dtype)
-#                }
-#            ]
-#        }   
-#        db.create_collection("anpr") 
-#        db.create_post(dump)
-#def imgs_data_read():        
-#        db = DataBase("anpr")
-#        db.create_collection("anpr") 
-#        for i in db.see_all_post():
-#            print (i["images"][0]['imageID'], "\n")
-#            
-#            gOut = db.file.get(i["images"][0]['imageID']) 
-#            img = np.frombuffer(gOut.read(), dtype=np.uint8)
+def imgs_data_write_v0():
+        img = cv2.imread("10700181-Image-1.jpeg")
+        imageID = db.file.put(img.tostring(), encoding='utf-8')
+        dump = {
+            'name': 'myTestSet',
+            'images': [
+                {
+                    'imageID': imageID,
+                    'shape': img.shape,
+                    'dtype': str(img.dtype)
+                }
+            ]
+        }   
+        db.create_collection("anpr") 
+        db.create_post(dump)
+        
+def imgs_data_read_v0():        
+        db = DataBase("anpr")
+        db.create_collection("anpr") 
+        for i in db.see_all_post():
+            gOut = db.file.get(i["images"][0]['imageID']) 
+            img = np.frombuffer(gOut.read(), dtype=np.uint8)
+            img = np.reshape(img, i["images"][0]['shape'])
+            imgs(img)  
 
-#            img = np.reshape(img, i["images"][0]['shape'])
-#            imgs(img)  
 
-db = DataBase("anpr")
 def imgs_data_write(x, answ, answ_sw, score, coord, coord_sw):
         name = str(uuid.uuid4())[:7]
-        print(name)
         imageID = db.file.put(x.tostring(), encoding='utf-8')
         dump = {
             'name': name,
@@ -170,56 +141,37 @@ def imgs_data_write(x, answ, answ_sw, score, coord, coord_sw):
         }   
         db.create_collection("anpr") 
         print(db.create_post(dump))
-#4c9f2f2
-#5f4c07afb2c79a42dc5d3390
+        
+        
 def imgs_data_read():
-        db = DataBase("anpr")
         db.create_collection("anpr") 
         for i in db.see_all_post():
-            #print (i)
-            if i["name"] == "name":
-               img = db.file.get(i["images"][0]["imageID"]).read()
+               img = db.file.get(ObjectId(i["images"])).read()
                nparr = np.fromstring(img, np.uint8)
                img_t = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
                imgs(img_t)
+               
           
  
             
-#https://coderoad.ru/49493493/Python-хранить-изображение-cv-в-mongodb-gridfs         
-#https://dev-gang.ru/article/integracija-mongodb-s-python-s-ispolzovaniem-pymongo-9hmv4a77cw/            
-#https://developer.mongodb.com/how-to/storing-large-objects-and-files
-#https://stackoverflow.com/questions/49493493/python-store-cv-image-in-mongodb-gridfs
-#from bs4 import BeautifulSoup
+print ("Start")
+db = DataBase("anpr")
+
+
 if __name__ == "__main__":
-        print ("Start")
-        db = DataBase("anpr")
-#--->        
-        db.create_collection("anpr") 
-        print (len(db.see_all_post()))
-        #check
-#        for i in db.see_all_post():
-#               print (i)
-#               img = db.file.get(i["images"]).read()
-#               nparr = np.fromstring(img, np.uint8)
-#               img_t = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#               im_name = 'check/'+i['name'] + ".jpg"
-#               cv2.imwrite(im_name, img_t) 
-#               file = open('check/'+i['name'] + ".txt","w")
-#               file.write(i["answ"])
-#               file.close()
-               #print (img_t.shape)
-               #imgs(img_t)
-        #images_data()
+       
+        #db.del_db_by_name("anpr") # Удалить базу данных
+        #db.create_collection("anpr")
+        #db.del_collection() # Удалить коллекцию
+        db.see_collection()
         
-#        col = db.see_collection()
-#        for i in col:
-#           db.create_collection(i)
-#           db.del_collection()
-
-        #t = db.see_collection()
-        #db.create_collection("files")
-        #print (db.see_all_post_v2(0,0, {"date":"27.08.2020 08:07:11"}))
+        #imgs_data_write_v0()
+        #imgs_data_read_v0()
+        
+        
+        
         
 
         
         
+
